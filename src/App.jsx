57b1aa14fa17bd22e8a63984cd03519585806b48
Vega <cesar.vega0597@gmail.com>
2026-02-16 21:21:07 -0600

    add new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
 import { useState} from 'react'
 
+
+function getLevel(counter) {
+  if (counter >= 10) return "High";
+  if (counter >= 5) return "Medium";
+  return "Low";
+}
+
+function getTextColor(counter) {
+  if (counter === 0) return "gray";
+  if (counter <= 4) return "green";
+  if (counter <= 9) return "orange";
+  return "red";
+}
+
+function getBackgroundColor(counter){
+    if(counter>10)return "#2c2c2c";
+    return "white";
+  }
+
+function getTitle(counter,level){
+    if(counter>10)return"Modo power";
+    return `Contador:${counter}(${level})`;
+
+  }
+
+function getReadableTextColor(backgroundColor){
+  if(backgroundColor === "white") return "black";
+  return "white"
+}
+
+//Inicio de aplicación  
 function App() {
   const[counter, setCounter] = useState(0)
   const[history, setHistory] = useState([])
-  const[text,setText] = useState("")
-
-  // 1) + 4 color dinamico (simple)
-  let color = "black";
-  if(counter === 0) color = "gray";
-  else if(counter <=4)color = "green";
-  else if (counter <= 9) color ="orange";
-  else color = "red";
-
-  // 2) Mensaje dinamico
-  let level = "Low";
-  if(counter >= 10) level = "High";
-  else if (counter >= 5) level = "Medium"
-
+  const level = getLevel(counter);
+  const color = getTextColor(counter);
+  const backColor = getBackgroundColor(counter);
+  const title = getTitle(counter, level);
+  const backColorReadable = getReadableTextColor(backColor)
+  
   //Suma
   function suma(){
-    setCounter(counter + 1)
-    setText("hola mundo")
+    //setCounter(counter + 1)
+    setCounter((prev)=>prev + 1)    
   }
   //Resta
   function resta(){
     if(counter > 0){
-      setCounter(counter - 1)
+      //setCounter(counter - 1)
+      setCounter((prev)=>prev-1);
     }
-    setText("Hola mundo 2")
   }
   //Reset
   function reset(){
@@ -52,11 +75,8 @@ function App() {
 
   return (
 
-    <div style={{fontFamily: "Arial", padding: 16}}>
-      <h1 style={{color}}>
-        Contador: {counter} 
-      </h1>
-      <h3>Prueba: {text}</h3>
+    <div style={{fontFamily: "Arial", padding: 16, backgroundColor:backColor}}>
+      <h1 style={{color}}>{title}</h1>
       <div>
         <button onClick={suma}> sumar </button>
         <button onClick={resta} disabled={counter===0}> Restar </button>
@@ -65,7 +85,7 @@ function App() {
         <button onClick={cleanHistory} disabled={history.length===0}> Clean history</button>
         <button onClick={removeLastone} disabled={history.length===0}> remove</button>
       </div>
-      <h3>Historial:</h3>
+      <h3 style={{color:backColorReadable}}>Historial:</h3>
       {history.length===0 ?(
         <p>No hay valores guardados.</p>
       ):(
